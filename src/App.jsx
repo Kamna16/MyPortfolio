ad2844b3cf8ddd2a87302d8078ee45386a81e6cb
kamna <kamnabhadoriya042@gmail.com>
2024-08-03 08:55:02 +0530

    removed mobile responsive error

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,7 @@ const App = () => {
   return (
     <div className='text-neutral-300 overflow-x-hidden antialised selection:bg-darkblue-25 selection:text-darkblue-400'>
       {/* <motion.div ref={ref} style={{ x, y }} className='box'></motion.div> */}
-      <div className='fixed top-0 -z-10 h-full w-full'>
-        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        </div>
-      </div>
+      
 
       <div className='mx-auto px-20'>
         <Navbar/>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='fixed top-0 -z-10 h-full w-full'>
+        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        </div>
+      </div>
     <App />
   </React.StrictMode>,
 )
