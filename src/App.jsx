bad616c85105893a7f48d87be474a190493352fe
kamna <kamnabhadoriya042@gmail.com>
2024-08-03 13:59:02 +0530

    error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   // const ref = useRef(null);
   // const { x, y } = useFollowPointer(ref);
   return (
-    <div className='text-neutral-300 overflow-x-hidden antialised selection:bg-darkblue-25 selection:text-darkblue-400'>
+    <div className='text-neutral-300 overflow-hidden antialised selection:bg-darkblue-25 selection:text-darkblue-400'>
       {/* <motion.div ref={ref} style={{ x, y }} className='box'></motion.div> */}
       
 
@@ -35,36 +35,3 @@ const App = () => {
 export default App
 
 
-
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Header from './components/Header';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
-// import Footer from './components/Footer';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className='flex flex-col bg-darkblue-600 text-white min-h-screen'>
-//         <Header />
-//         <div className="bg-darkblue-600 text-white flex-1">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//         </div>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
